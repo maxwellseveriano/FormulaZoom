359d922ce48968f6942bc7741ded9a35ce163884
Maxwell William <maxwellseveriano91@gmail.com>
2024-07-06 17:20:29 -0300

    reinicio de tudo

---
+++ CharacterBody2D.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animation:= $AnimatedSprite2D
+var is_jumping:= false
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
+		
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction != 0:
+		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if not is_jumping:
+			animation.play("Run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("default")
+	
+	if is_jumping:
+		animation.play("Jump")
+
+	move_and_slide()
