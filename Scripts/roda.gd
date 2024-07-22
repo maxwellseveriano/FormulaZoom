17d02fb896d82f4fadca159b46bfe6e92a16030c
Davi Cardoso <davideoliveira2003@gmail.com>
2024-07-22 20:34:26 -0300

    Setando os Pneus

--- Scripts/roda.gd
+++ Scripts/roda.gd
@@ -6,10 +6,12 @@ const JUMP_VELOCITY = -400.0
 
 @onready var wall_detector := $wall_detector as RayCast2D
 @onready var texture := $texture as Sprite2D
+@onready var damage_sfx = $player_sounds/damage_sfx as AudioStreamPlayer
 
 var direction := -1
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var roda_life = 5
 
 
 func _physics_process(delta):
@@ -23,5 +25,15 @@ func _physics_process(delta):
 		texture.flip_h = !texture.flip_h
 
 	velocity.x = direction * SPEED * delta
-
 	move_and_slide()
+
+func decrementar_roda():
+	roda_life = roda_life -1
+	var color_roda_tween := get_tree().create_tween()
+	damage_sfx.play()
+	texture.modulate = Color(1, 0, 0, 1)
+	color_roda_tween.parallel().tween_property(texture, "modulate", Color(1, 1, 1, 1), 0.35)
+	
+func printar_life():
+	print (roda_life)
+	
