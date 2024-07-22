a50dd353d2e034695ae1475545f13473b71fe661
maxwellseveriano <maxwellseveriano91@gmail.com>
2024-07-22 00:02:55 -0300

    correcao de bugs

--- Singletons/globals.gd
+++ Singletons/globals.gd
@@ -4,11 +4,14 @@ var moedas := 0
 var score := 0
 var player_life := 3
 var player = null
+var player_start_position = null
 var current_checkpoint = null
 
 func respawn_player():
 	if current_checkpoint != null:
 		player.position = current_checkpoint.global_position
+	else:
+		player.global_position = player_start_position.global_position
 
 
 func _ready():
